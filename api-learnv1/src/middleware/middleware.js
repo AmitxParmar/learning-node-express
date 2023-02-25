bb05f8cd67eccf5fb6b05828b71f09e8dac6e3a1
AmitxParmar <amitparmar901@gmail.com>
2023-02-26 01:20:07 +0530

    complete middleware, doing routes and server

--- api-learnv1/src/middleware/middleware.js
+++ api-learnv1/src/middleware/middleware.js
@@ -9,3 +9,26 @@ const authControl = (req, res, next) => {
         })
     }
 }
+
+const banControl = (req, res, next) => {
+    const isBanned = false;
+    if (isBanned) {
+        res.json({
+            success: false,
+            message: "You are banned user!"
+        })
+    } else {
+        next();
+    }
+}
+
+const homeControl = (req, res, next) => {
+    console.log("Home control")
+    next();
+}
+
+module.exports = {
+    authControl,
+    banControl,
+    homeControl,
+}
